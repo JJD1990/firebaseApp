1368a1bc8d5f07cc35eb0fb5b91efa6442450944
jjd1990 <jack.douglas@rocketmail.com>
2022-05-16 21:22:59 +0100

    server up and running, css and firebase linked

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
-const 
+const app = express();
+
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
